7882919308d9fa40da2fb1f11f12e7f45af0e384
FrantiskaM <frantiska.malucka@gmail.com>
2024-07-18 18:15:54 +0200

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generatePlan(event) {
-  event.preventDefault();
-
+function displayPlan(response) {
+  console.log("plan generated");
   new Typewriter("#plan", {
-    strings: "Plan will be here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+function generatePlan(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "o5ea5d7b5d6e56d8c97c0faeb8283t04";
+  let prompt = `User instructions: Generate ${instructionsInput.value} workout plan`;
+  let context =
+    "You are a fitness Plan expert and you love to plan workout sessions. Your mission is to generate a plan basic HTML. Make sure to follow the user instructions. Sign the plan with 'Created by SheCodes AI' writed in cursive and bold";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating plan");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayPlan);
 }
 
 let planFormElement = document.querySelector("#workout-plan-generator-form");
